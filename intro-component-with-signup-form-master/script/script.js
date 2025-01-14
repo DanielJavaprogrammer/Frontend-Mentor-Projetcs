d4dc24e278c9dbe3f11d97c1e810f2d2efbecf03
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2025-01-13 22:51:14 -0300

    ok

--- intro-component-with-signup-form-master/script/script.js
+++ intro-component-with-signup-form-master/script/script.js
@@ -5,22 +5,40 @@ enviar.addEventListener('click', () => {
     const sobrenome = document.getElementById('lastName');
     const email = document.getElementById('email');
     const senha = document.getElementById('password');
+    const imgError = document.querySelector('.img_erro');
 
     const inputFname = nome.value.trim();
     const inputLname = sobrenome.value.trim();
     const emailinput = email.value.trim();
     const inputsenha = senha.value.trim();
 
-    function error() {
-        input.style.borderColor = 'red'
-    }
-
     function error(input) {
         input.style.borderColor = 'red'; // Adiciona uma borda vermelha ao campo
     }
 
+    function imgErro(img){
+        img.style.display = 'block';
+    }
+
+
     // Valida os campos
     if (inputFname === '') {
-        error(nome); // Aplica o erro no campo 'nome'
+        error(nome);
+        imgErro(imgError)
+    }
+
+    if(inputLname === '') {
+        error(sobrenome); 
+        imgErro(imgError)
+    }   
+
+    if(emailinput === '') {
+        error(email); 
+        imgErro(imgError)
+    }
+
+    if(inputsenha === '') {
+        error(senha); 
+        imgErro(imgError)
     }
 })  
